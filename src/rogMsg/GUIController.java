035d284d3a9eb1773370071ca535b3a3423781c5
mrcassani051819 <mr.cassani051819@gmail.com>
2017-11-27 13:06:01 -0500

    Started FXML form for main chat window
    Converting the ugly swing main window to FXML because it looks nicer and is way less of a headache to work with.

--- src/rogMsg/GUIController.java
+++ src/rogMsg/GUIController.java
@@ -8,6 +8,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -15,6 +18,13 @@ import javafx.stage.Stage;
 public class GUIController
 {
 	@FXML private Text actionTarget;
+	@FXML private Text groupName;
+	@FXML private ScrollPane userList;
+	@FXML private TabPane announcements;
+	@FXML private TabPane extraControls;
+	@FXML private Tab lists;
+	@FXML private Tab polls;
+	@FXML private Tab announcementsTab;
 	@FXML private Button submitButton;
 	@FXML private Button registerButton;
 	@FXML private Button cancelButton;
@@ -28,9 +38,13 @@ public class GUIController
 		if(e.getSource().equals(submitButton))
 		{
 //			actionTarget.setText("Not implemented yet.");
-			ChatWindow cw = new ChatWindow();
-			Stage s = (Stage)submitButton.getScene().getWindow();
-			s.close();
+//			ChatWindow cw = new ChatWindow();
+			Parent p = FXMLLoader.load(getClass().getResource("ChatWindowFXML.fxml"));
+			Scene s = new Scene(p, 400, 375);
+			Stage ps = (Stage)submitButton.getScene().getWindow();
+			ps.setTitle("ROG-Msg - Group Name");
+			ps.setScene(s);
+//			s.close();
 		}
 		else if(e.getSource().equals(registerButton))
 		{
